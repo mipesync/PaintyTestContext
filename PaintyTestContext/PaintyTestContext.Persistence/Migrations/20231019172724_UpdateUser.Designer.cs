9c748d3269f7b6090adf43dbe2e3e11611bd77c5
Аркадий Алфёров <arhe414@gmail.com>
2023-10-19 20:37:46 +0300

    feature: Сделал миграцию с изменёнными типами полей пользователя

--- PaintyTestContext/PaintyTestContext.Persistence/Migrations/20231019172724_UpdateUser.Designer.cs
+++ PaintyTestContext/PaintyTestContext.Persistence/Migrations/20231019172724_UpdateUser.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PaintyTestContext.Persistence;
@@ -11,9 +12,11 @@ using PaintyTestContext.Persistence;
 namespace PaintyTestContext.Persistence.Migrations
 {
     [DbContext(typeof(DBContext))]
-    partial class DBContextModelSnapshot : ModelSnapshot
+    [Migration("20231019172724_UpdateUser")]
+    partial class UpdateUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
